54c0a500d14e9966a4bcaac7b588bf72d6d8353e
Keane Moraes <lordvader3002@gmail.com>
2024-04-04 03:12:34 -0400

    added methods to varialbe and grad structs

--- src/tape.rs
+++ src/tape.rs
@@ -5,6 +5,12 @@ struct Node {
     deps: [usize; 2],
 }
 
+impl Node {
+    pub fn new(weight: [f64; 2], deps: [usize; 2]) -> Self {
+        Self { weight, deps }
+    }
+}
+
 pub(crate) struct Tape {
     nodes: RefCell<Vec<Node>>,
 }
--- src/variable.rs
+++ src/variable.rs
@@ -1,12 +1,25 @@
+use crate::grad::Gradient;
 use crate::tape::Tape;
 
 pub struct Variable<'a> {
-    tape: &'a Tape,
+    tape: Option<&'a Tape>,
     index: usize,
     value: f64,
 }
 
 impl<'a> Variable<'a> {
+    pub fn from(value: f64) -> Self {
+        Self {
+            tape: None,
+            index: 0,
+            value: value,
+        }
+    }
+
+    pub fn item(&self) -> f64 {
+        self.value
+    }
+
     pub fn index(&self) -> usize {
         self.index
     }
